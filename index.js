ad63dac3306fed5474ca08507d7b791ad14b9599
blake <blake.fang@letote.cn>
2018-11-01 17:35:22 +0800

    update version1.1.2

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
+import { NativeModules } from 'react-native'
 
-import { NativeModules } from 'react-native';
-
-const { RNJdpay } = NativeModules;
-
-export default RNJdpay;
+const { RNJdpay } = NativeModules
+class JdPay {
+  pay = async authorization_details => {
+    try {
+      let authorObject = JSON.parse(authorization_details)
+      let result = await RNJdpay.pay(authorObject.jdpay)
+      if (typeof result === 'string') {
+        result = JSON.parse(result)
+      }
+      if (result.payStatus === 'JDP_PAY_SUCCESS') {
+        return this.sendPaySuccess()
+      } else {
+        return this.sendPayFail(result)
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
+  sendPaySuccess = () => {
+    const result = {}
+    result.errCode = 0
+    return result
+  }
+  sendPayFail = data => {
+    const result = {}
+    //支付失败
+    result.errCode = -1
+    //京东返回的errorcode
+    if (data && data.errorCode) {
+      result.error = data.errorCode
+    }
+    return result
+  }
+}
+JdPay = new JdPay()
+export default JdPay
